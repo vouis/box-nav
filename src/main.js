765a71ecf702e307daff5ccd5b8df9786e535687
dingning <2667752905@qq.com>
2021-02-26 16:59:15 +0800

    feat: add four background img

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
+import bg1 from './assets/bgImage/bg1.jpg';
+import bg2 from './assets/bgImage/bg2.jpg';
+import bg3 from './assets/bgImage/bg3.jpg';
+import bg4 from './assets/bgImage/bg4.jpg';
+
 const $siteList = $('.siteList');
 const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
+const imgArray = [null,bg1,bg2,bg3,bg4]
+let i=1;
+
 const hashMap = xObject || [
     { logo: 'B', logoType: 'text', url: 'https://www.bilibili.com' },
     { logo: 'M', logoType: 'image', url: 'https://mail.google.com' },
     { logo: 'B', logoType: 'image', url: 'https://baidu.com' },
     { logo: 'I', logoType: 'image', url: 'https://www.iconfont.cn' },
     { logo: 'C', logoType: 'image', url: 'https://www.cnki.net' },
-    { logo: 'J', logoType: 'image', url: 'https://juejin.im' }
+    { logo: 'J', logoType: 'image', url: 'https://juejin.im' },
+    {logo: 'Y', logoType: 'image', url: 'https://www.yuque.com'}
 ]
 
 const removeHttps = (url) => {
@@ -54,15 +63,22 @@ $('.add')
         let url = window.prompt('请输入网址：')
         if (url.indexOf('http') !== 0) {
             url = 'https://' + url;
-            //console.log(url)
         }
 
         hashMap.push({ logo: removeHttps(url)[0].toUpperCase(), logoType: 'image', url: url })
         render();
     });
 
+
+$('.changeTheme')
+    .on('click', () => {
+        let showImg = imgArray[i%imgArray.length];
+        $('body').css('background-image', `url(${showImg})`);
+        i++;
+    });
+
+
 window.onbeforeunload = () => {
-    //console.log('uemianguyanbi1')
     const string = JSON.stringify(hashMap)
     localStorage.setItem('x', string)
 }
